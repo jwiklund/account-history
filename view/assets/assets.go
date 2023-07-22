c61bae42c51d562349e8b3bb9d330a8f57a3223c
Jonas Wiklund <jwiklund@gmail.com>
2023-07-22 12:07:40 +0200

    Fix view assets

--- view/assets/assets.go
+++ view/assets/assets.go
@@ -2,9 +2,5 @@ package assets
 
 import "embed"
 
-//go:embed edit.html
-//go:embed head.html
-//go:embed index.html
-//go:embed nav.html
-//go:embed scripts.html
+//go:embed *.html
 var EmbedFs embed.FS
